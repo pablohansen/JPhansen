db0656fb3da387731ad587a920f4f5fdc5b62fdc
pablohansen <70018307+pablohansen@users.noreply.github.com>
2023-03-07 22:30:50 -0300

    Upload files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showForm() {
+    var form = document.getElementById("contact-form");
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  }
+  
